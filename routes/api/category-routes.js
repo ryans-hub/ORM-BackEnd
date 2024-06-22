4a614eb2b77a727f069ef29dec2a17ca2c30a130
PanMan <ryanispan@gmail.com>
2024-06-22 11:07:03 -0700

    some category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,35 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model:Product}],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json({error: 'Error fetching categories'});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryId = req.params.id;
+    const categoryData = await Category.findByPk(categoryId, {
+      include: [{model:Product}],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({error: 'Category not found '});
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json({error: 'Server Error'});
+  }
 });
 
 router.post('/', (req, res) => {
